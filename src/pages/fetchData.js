b4c126d7e8c99d157f65209efc9db07fb1485be3
Osezei <john.osezei@yahoo.com>
2022-07-27 00:02:47 +0100

    first commit

---
+++ src/pages/fetchData.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const url =
+  "https://static.openfoodfacts.org/data/openfoodfacts-products.jsonl.gz";
+// const url = "http://hn.algolia.com/api/v1/search?query=";
+
+const FetchData = () => {
+  const fetchRequest = async () => {
+    try {
+      const response = await axios(url);
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  useEffect(() => {
+    fetchRequest();
+  }, []);
+
+  return (
+    <div>
+      <h1> hello</h1>
+    </div>
+  );
+};
+
+export default FetchData;
